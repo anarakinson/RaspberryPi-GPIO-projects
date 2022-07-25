9aebc4eba0462464d797e52481aed43062899f1b
anarakinson <anarakinson@gmail.com>
2022-07-25 22:29:17 +0300

    fix get_val.py

--- get_val.py
+++ get_val.py
@@ -1,10 +1,19 @@
 import RPi.GPIO as gpio
 import time
 
-gpio.setmode(gpio.BCM)
-gpio.setup(21, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+
+def get_val():
+    gpio.setmode(gpio.BCM)
+    gpio.setup(21, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
 
-while True:
-    time.sleep(2)
-    print(float(gpio.input(21)))
+    while True:
+        time.sleep(2)
+        print(float(gpio.input(21)))
+
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        gpio.cleanup()
+        print("Keyboard interrupt")
