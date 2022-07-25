d084553cfd42225a3845f6bfe9afc541f3154cb4
anarakinson <anarakinson@gmail.com>
2022-07-25 22:21:15 +0300

    add servo-pigpio.py

--- servo-pigpio.py
+++ servo-pigpio.py
@@ -3,42 +3,54 @@ import RPi.GPIO as GPIO
 import pigpio
 import time
 import subprocess
+import argparse
 
-# start pigpiod
-# subprocess.run(["sudo", "pigpiod"])
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--angle", help="set angle", default="90")
+    parser.add_argument("-p", "--pin", help="set servo pin", default="26")
+    args = parser.parse_args()
+    return args
+
+def main():
+    # start pigpiod
+    # subprocess.run(["sudo", "pigpiod"])
  
-servo = 26
+    args = parse_args()
+
+    servo = int(args.pin)
+    angle = int(args.angle)
+
+    if angle > 180:
+        angle = 180
+    if angle  < 0:
+        angle = 0
+    print(angle)
+    angle = ((angle / 90) * 1000) + 500
  
-# more info at http://abyz.me.uk/rpi/pigpio/python.html#set_servo_pulsewidth
+    # more info at http://abyz.me.uk/rpi/pigpio/python.html#set_servo_pulsewidth
  
-pwm = pigpio.pi() 
-pwm.set_mode(servo, pigpio.OUTPUT)
+    pwm = pigpio.pi() 
+    pwm.set_mode(servo, pigpio.OUTPUT)
 
-pwm.set_PWM_frequency( servo, 50 )
+    pwm.set_PWM_frequency( servo, 50 )
 
-#print( "0 deg" )
-#pwm.set_servo_pulsewidth( servo, 500 )
-#time.sleep( 3 )
 
-#print( "90 deg" )
-#pwm.set_servo_pulsewidth( servo, 1500 )
-#time.sleep( 3 )
+#    for i in range(500, 2501, 5):
+#        #print(f"freqency - {i}")
+#        pwm.set_servo_pulsewidth( servo, i )
+#        time.sleep(0.01)
 
-#print("180 deg")
-#pwm.set_servo_pulsewidth( servo, 2500 )
-#time.sleep(3)
+#    print("return to 90 degree")
+    pwm.set_servo_pulsewidth(servo, angle)
+    time.sleep(1)
 
-print("rotate 180 degree")
-for i in range(500, 2501, 5):
-    #print(f"freqency - {i}")
-    pwm.set_servo_pulsewidth( servo, i )
-    time.sleep(0.01)
+    # turning off servo
+    print("turning off")
+    pwm.set_PWM_dutycycle(servo, 0)
+    pwm.set_PWM_frequency( servo, 0 )
 
-print("return to 90 degree")
-pwm.set_servo_pulsewidth(servo, 1500)
-time.sleep(1)
-
-# turning off servo
-print("turning off")
-pwm.set_PWM_dutycycle(servo, 0)
-pwm.set_PWM_frequency( servo, 0 )
+if __name__ == "__main__":
+    main()
